dd5c5683dcb7489e1a2f38677b7726dc25e73781
Mike F <mikefolcher@gmail.com>
2014-04-23 21:08:21 -0400

    Initial Commit

---
+++ MMFFlashCardTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MMFFlashCardTime
+//
+//  Created by Michael Folcher on 4/23/14.
+//  Copyright (c) 2014 Michael Folcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMFAppDelegate class]));
+    }
+}
