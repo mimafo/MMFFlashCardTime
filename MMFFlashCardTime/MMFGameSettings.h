c40202b67115518dd93a5ed85bb71f0ea594ab90
Mike F <mikefolcher@gmail.com>
2014-05-23 23:25:23 -0400

    - Create MMFGameSettings class to hold the user settings - Use NSUserDefaults as the backing datasource

---
+++ MMFFlashCardTime/MMFGameSettings.h
@@ -0,0 +1,30 @@
+//
+//  MMFGameSettings.h
+//  MMFFlashCardTime
+//
+//  Created by Michael Folcher on 5/23/14.
+//  Copyright (c) 2014 Michael Folcher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum {
+    kEasy,
+    kMedium,
+    kDifficult
+} GameLevel;
+
+@interface MMFGameSettings : NSObject
+
+@property (nonatomic, copy) NSString *userName;
+@property (nonatomic, assign) GameLevel level;
+@property (nonatomic, assign) NSUInteger gameDuration; //In minutes
+@property (nonatomic, assign) NSUInteger highScore; //Answers per minute
+
+-(void)save;
+-(id)initWithDataSource;
+
++(instancetype)sharedGameSettings;
+
+
+@end
---
+++ MMFFlashCardTime/MMFGameSettings.m
@@ -0,0 +1,91 @@
+//
+//  MMFGameSettings.m
+//  MMFFlashCardTime
+//
+//  Created by Michael Folcher on 5/23/14.
+//  Copyright (c) 2014 Michael Folcher. All rights reserved.
+//
+
+#import "MMFGameSettings.h"
+
+//String constants
+static NSString * const GameSettingsKey = @"GameSettingsKey";
+static NSString * const UserNameKey = @"UserNameKey";
+static NSString * const LevelKey = @"LevelKey";
+static NSString * const GameDurationKey = @"GameDurationKey";
+static NSString * const HighScoreKey = @"HighScoreKey";
+
+@interface MMFGameSettings()
+
+@property (nonatomic, strong) NSMutableDictionary *dataSource;
+
+@end
+
+@implementation MMFGameSettings
+
+#pragma mark - Class methods
++(instancetype)sharedGameSettings;
+{
+    static MMFGameSettings *_settings = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        _settings = [MMFGameSettings new];
+        
+    });
+    return _settings;
+}
+
+#pragma mark - Method getters and setters
+-(NSMutableDictionary *)dataSource
+{
+    if (_dataSource == nil) {
+        _dataSource = [[NSUserDefaults standardUserDefaults] objectForKey:GameSettingsKey];
+        if (_dataSource == nil) {
+            _dataSource = [NSMutableDictionary dictionary];
+            [self save];
+        }
+    }
+    return _dataSource;
+}
+
+#pragma mark - Initializers
+-(id)init
+{
+    self = [super init];
+    if (self) {
+        self.level = kEasy;
+        self.gameDuration = 2;
+        self.highScore = 0;
+    }
+    return self;
+}
+
+-(id)initWithDataSource
+{
+    self = [self init];
+    
+    self.userName = self.dataSource[UserNameKey];
+    self.level = [self.dataSource[LevelKey] unsignedIntegerValue];
+    self.gameDuration = [self.dataSource[GameDurationKey] unsignedIntegerValue];
+    self.highScore = [self.dataSource[HighScoreKey] unsignedIntegerValue];
+    
+    return self;
+}
+
+#pragma mark - Public Methods
+-(void)save
+{
+    self.dataSource[UserNameKey] = self.userName;
+    self.dataSource[LevelKey] = @(self.level);
+    self.dataSource[GameDurationKey] = @(self.gameDuration);
+    self.dataSource[HighScoreKey] = @(self.highScore);
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:[self.dataSource copy] forKey:GameSettingsKey];
+    [defaults synchronize];
+}
+
+
+
+@end
